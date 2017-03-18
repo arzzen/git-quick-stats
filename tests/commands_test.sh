549438865548dd01180f5d27fe7112c116f8635f
Lukas Mestan <lukas.mestan@gmail.com>
2017-03-18 14:04:03 +0100

    fix exit code

--- tests/commands_test.sh
+++ tests/commands_test.sh
@@ -5,7 +5,7 @@
 src="./git-quick-stats"
 
 assert "$src fail" "Invalid argument. Possible arguments: suggestReviewers, detailedGitStats, commitsPerDay, commitsByMonth, commitsByWeekday, commitsByHour, commitsByAuthorByHour, commitsPerAuthor, myDailyStats, contributors, branchTree, branchesByDate, changelogs"
-assert_raises "$src fail" 0
+assert_raises "$src fail" 1
 
 assert_contains "$src suggestReviewers" "Suggested code reviewers (based on git history)" 127
 assert_raises "$src suggestReviewers" 0
