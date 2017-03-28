515fdb3b083f5d274b8d765b5e53e33c273a77a8
Lukáš Mešťan <arzzen@users.noreply.github.com>
2017-03-28 11:46:40 +0200

    fix invalid argument test

--- tests/commands_test.sh
+++ tests/commands_test.sh
@@ -4,7 +4,7 @@
 
 src="./git-quick-stats"
 
-assert "$src fail" "Invalid argument. Possible arguments: suggestReviewers, detailedGitStats, commitsPerDay, commitsByMonth, commitsByWeekday, commitsByHour, commitsByAuthorByHour, commitsPerAuthor, myDailyStats, contributors, branchTree, branchesByDate, changelogs"
+assert "$src fail" "Invalid argument. Possible arguments: suggestReviewers, detailedGitStats, commitsPerDay, commitsByMonth, commitsByWeekday, commitsByHour, commitsByAuthorByHour, commitsPerAuthor, myDailyStats, contributors, branchTree, branchesByDate, changelogs, changelogsByAuthor"
 assert_raises "$src fail" 1
 
 assert_contains "$src suggestReviewers" "Suggested code reviewers (based on git history)" 127
